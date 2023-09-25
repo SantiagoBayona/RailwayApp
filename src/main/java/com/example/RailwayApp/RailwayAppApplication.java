98b43c6c2ba2409d0cdda01b3123a5d9450e9039
Santiago Bayona <santiago.bayona@mail.escuelaing.edu.co>
2023-09-24 23:28:42 -0500

    first commit

---
+++ src/main/java/com/example/RailwayApp/RailwayAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.RailwayApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayAppApplication.class, args);
+	}
+
+}
